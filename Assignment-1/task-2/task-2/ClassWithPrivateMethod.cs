ab0c98f285763bf7157524e545c690df17c8bf72
Rafiqul Iislam Robin <kratosrobin467@gmail.com>
2021-05-14 16:52:06 +0600

    .

---
+++ Assignment-1/task-2/task-2/ClassWithPrivateMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace task_2
+{
+    public class ClassWithPrivateMethod
+    {
+        private void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+}
---
+++ Assignment-1/task-2/task-2/ReflectionUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace task_2
+{
+    public class ReflectionUtility
+    {
+        public void CallPrivate(object targetObject, string methodName, object[] args)
+        {
+            var method = typeof(ClassWithPrivateMethod).GetMethod(
+                           methodName,
+                           BindingFlags.NonPublic
+                         | BindingFlags.Instance
+                         | BindingFlags.DeclaredOnly);
+
+            method.Invoke(targetObject, args);
+
+
+
+        }  
+    }
+
+}
