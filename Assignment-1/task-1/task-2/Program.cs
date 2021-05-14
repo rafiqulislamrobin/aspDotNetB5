f8be138da78f0046e3b319660cb42aeb57c6ffd4
Rafiqul Iislam Robin <kratosrobin467@gmail.com>
2021-05-14 16:24:41 +0600

    ASSIGNMENT-1

---
+++ Assignment-1/task-1/task-2/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    public class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            var configBuilder = new
+            ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+
+            
+            Log.Logger = new LoggerConfiguration()
+                 .WriteTo
+                 .MSSqlServer(
+                     connectionString: @"Server=DESKTOP-QO6CDJ4\SQLEXPRESS; Database=SeriDB; Integrated Security=SSPI;",
+                     sinkOptions: new MSSqlServerSinkOptions { TableName = "Logs" })
+                 .CreateLogger();
+
+
+            try
+            {
+                Log.Information("Application Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+          public static IHostBuilder CreateHostBuilder(string[] args) =>
+              Host.CreateDefaultBuilder(args)
+              .UseSerilog()
+              .ConfigureWebHostDefaults(webBuilder =>
+              {
+                 webBuilder.UseStartup<Startup>();
+                 webBuilder.UseUrls("http://*:80");
+              });
+
+    }
+}
--- Assignment-1/task-2/task-2/ReflectionUtility.cs
+++ Assignment-1/task-2/task-2/ReflectionUtility.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Reflection;
 
-
 namespace task_2
 {
     public class ReflectionUtility
     {
         public void CallPrivate(object targetObject, string methodName, object[] args)
-
         {
-            var method = typeof(ClassWithPrivateMethod).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            var method = typeof(ClassWithPrivateMethod).GetMethod(
+                           methodName,
+                           BindingFlags.NonPublic
+                         | BindingFlags.Instance
+                         | BindingFlags.DeclaredOnly);
 
             method.Invoke(targetObject, args);
+
+
 
         }  
     }
