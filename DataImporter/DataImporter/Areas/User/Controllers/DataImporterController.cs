3eec993a45b3d2da35d4d15f0772aa95f1d4d721
Rafiqul Iislam Robin <kratosrobin467@gmail.com>
2021-09-06 18:52:05 +0600

    UI

---
+++ DataImporter/DataImporter/Areas/User/Controllers/DataImporterController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataImporter.Areas.User.Controllers
+{
+    [Area("User")]
+    public class DataImporterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ViewGroups()
+        {
+            return View();
+        }
+        public IActionResult CreateGroups()
+        {
+            return View();
+        }
+        public IActionResult ViewContacts()
+        {
+            return View();
+        }
+        public IActionResult CreateContacts()
+        {
+            return View();
+        }
+
+        public IActionResult ImportFile()
+        {
+            return View();
+        }
+        public IActionResult ViewImportFile()
+        {
+            return View();
+        }
+        public IActionResult ImportHistory()
+        {
+            return View();
+        }
+        public IActionResult ExportFile()
+        {
+            return View();
+        }
+        public IActionResult ExportFileHistory()
+        {
+            return View();
+        }
+    }
+}
